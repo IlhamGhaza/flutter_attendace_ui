029e131dc7ece37614dac3c4146b669feb5695e2
Ghazali <57607688+IlhamGhaza@users.noreply.github.com>
2024-08-20 19:43:34 +0700

    Update home_page.dart

--- lib/ui/home/pages/home_page.dart
+++ lib/ui/home/pages/home_page.dart
@@ -1,204 +1,141 @@
 import 'package:flutter/material.dart';
-
 import '../../../core/core.dart';
-import '../widgets/menu_button.dart';
-import 'attendance_page.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: Assets.images.bgHome.provider(),
-              alignment: Alignment.topCenter,
-            ),
-          ),
-          child: ListView(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            children: [
-              Row(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(50.0),
-                    child: Image.network(
-                      'https://i.pinimg.com/originals/1b/14/53/1b14536a5f7e70664550df4ccaa5b231.jpg',
-                      width: 48.0,
-                      height: 48.0,
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  height: 250.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: Assets.images.bgHome.provider(),
+                      alignment: Alignment.topCenter,
                       fit: BoxFit.cover,
                     ),
                   ),
-                  const SpaceWidth(12.0),
-                  const Expanded(
-                    child: Text(
-                      'Hello, Chopper Sensei',
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        color: AppColors.white,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 40.0, left: 16.0),
+                  child: Row(
+                    children: [
+                      IconButton(
+                        icon: const Icon(
+                          Icons.arrow_back,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
                       ),
-                      maxLines: 2,
+                      const SizedBox(width: 16.0),
+                      const Text(
+                        'Profile',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 120.0,
+                  left: MediaQuery.of(context).size.width / 2 - 50.0,
+                  child: CircleAvatar(
+                    radius: 50.0,
+                    backgroundImage: const NetworkImage(
+                      'https://i.pinimg.com/originals/1b/14/53/1b14536a5f7e70664550df4ccaa5b231.jpg',
+                    ),
+                    child: Align(
+                      alignment: Alignment.bottomRight,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 15.0,
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.edit,
+                            color: Colors.blue,
+                            size: 15.0,
+                          ),
+                          onPressed: () {
+                            // Aksi edit foto profil
+                          },
+                        ),
+                      ),
                     ),
                   ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Assets.icons.notificationRounded.svg(),
+                ),
+                Positioned(
+                  top: 220.0,
+                  left: 0,
+                  right: 0,
+                  child: Center(
+                    child: Column(
+                      children: const [
+                        Text(
+                          'hera@geoface.id | +6285806391116',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.0,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ],
-              ),
-              const SpaceHeight(24.0),
-              Container(
-                padding: const EdgeInsets.all(24.0),
-                decoration: BoxDecoration(
-                  color: AppColors.white,
-                  borderRadius: BorderRadius.circular(20.0),
                 ),
-                child: Column(
-                  children: [
-                    Text(
-                      DateTime.now().toFormattedTime(),
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 32.0,
-                        color: AppColors.primary,
-                      ),
-                    ),
-                    Text(
-                      DateTime.now().toFormattedDate(),
-                      style: const TextStyle(
-                        color: AppColors.grey,
-                        fontSize: 12.0,
-                      ),
-                    ),
-                    const SpaceHeight(18.0),
-                    const Divider(),
-                    const SpaceHeight(30.0),
-                    Text(
-                      DateTime.now().toFormattedDate(),
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w600,
-                        color: AppColors.grey,
-                      ),
-                    ),
-                    const SpaceHeight(6.0),
-                    Text(
-                      '${DateTime(2024, 3, 14, 8, 0).toFormattedTime()} - ${DateTime(2024, 3, 14, 16, 0).toFormattedTime()}',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SpaceHeight(80.0),
-              GridView(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: 30.0,
-                  mainAxisSpacing: 30.0,
-                ),
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
+              ],
+            ),
+            const SizedBox(height: 30.0),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  MenuButton(
-                    label: 'Datang',
-                    iconPath: Assets.icons.menu.datang.path,
-                    onPressed: () {},
+                  const Text(
+                    'Account',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                  MenuButton(
-                    label: 'Pulang',
-                    iconPath: Assets.icons.menu.pulang.path,
-                    onPressed: () {},
+                  const SizedBox(height: 10.0),
+                  ListTile(
+                    title: const Text('Edit Profile'),
+                    trailing: const Icon(Icons.arrow_forward_ios),
+                    onTap: () {
+                      // Aksi untuk edit profil
+                    },
                   ),
-                  MenuButton(
-                    label: 'Jadwal',
-                    iconPath: Assets.icons.menu.jadwal.path,
-                    onPressed: () {},
+                  const Divider(),
+                  ListTile(
+                    title: const Text('Jabatan'),
+                    trailing: const Icon(Icons.arrow_forward_ios),
+                    onTap: () {
+                      // Aksi untuk jabatan
+                    },
                   ),
-                  MenuButton(
-                    label: 'Izin',
-                    iconPath: Assets.icons.menu.izin.path,
-                    onPressed: () {},
-                  ),
-                  MenuButton(
-                    label: 'Lembur',
-                    iconPath: Assets.icons.menu.lembur.path,
-                    onPressed: () {},
-                  ),
-                  MenuButton(
-                    label: 'Catatan',
-                    iconPath: Assets.icons.menu.catatan.path,
-                    onPressed: () {},
+                  const Divider(),
+                  ListTile(
+                    title: const Text('Perangkat Terdaftar'),
+                    trailing: const Icon(Icons.arrow_forward_ios),
+                    onTap: () {
+                      // Aksi untuk perangkat terdaftar
+                    },
                   ),
                 ],
               ),
-              const SpaceHeight(24.0),
-              Button.filled(
-                onPressed: () {
-                  showBottomSheet(
-                    backgroundColor: AppColors.white,
-                    context: context,
-                    builder: (context) => Container(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          const SizedBox(
-                            width: 60.0,
-                            height: 8.0,
-                            child: Divider(color: AppColors.lightSheet),
-                          ),
-                          const CloseButton(),
-                          const Center(
-                            child: Text(
-                              'Oops !',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontSize: 24.0,
-                              ),
-                            ),
-                          ),
-                          const SpaceHeight(4.0),
-                          const Center(
-                            child: Text(
-                              'Aplikasi ingin mengakses Kamera',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontSize: 15.0,
-                              ),
-                            ),
-                          ),
-                          const SpaceHeight(36.0),
-                          Button.filled(
-                            onPressed: () => context.pop(),
-                            label: 'Tolak',
-                            color: AppColors.secondary,
-                          ),
-                          const SpaceHeight(16.0),
-                          Button.filled(
-                            onPressed: () {
-                              context.pop();
-                              context.push(const AttendancePage());
-                            },
-                            label: 'Izinkan',
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-                label: 'Attendance Using Face ID',
-                icon: Assets.icons.attendance.svg(),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
